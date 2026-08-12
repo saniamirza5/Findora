601ac47e3b236a8d4214bbbd00c785966f7e573a
saniamirza5 <saniamirza9336@gmail.com>
2026-08-12 16:14:53 +0530

    Allow production frontend CORS

--- backend/src/main/java/com/campuslostfound/campus_lost_found_backend/config/SecurityConfig.java
+++ backend/src/main/java/com/campuslostfound/campus_lost_found_backend/config/SecurityConfig.java
@@ -34,7 +34,10 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
 
         configuration.setAllowedOrigins(
-                List.of("http://localhost:5173")
+                List.of(
+                        "http://localhost:5173",
+                        "https://findora-zeta.vercel.app"
+                )
         );
 
         configuration.setAllowedMethods(
